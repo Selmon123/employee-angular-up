3e86c25ce99dcbf47ffd63aeac14f6beaf4910a0
Selmon123 <sudhil.sujith7384@gmail.com>
2022-07-02 15:34:40 +0530

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddemployeeComponent
+    AddemployeeComponent,
+    SearchEmployeeComponent
   ],
   imports: [
     BrowserModule,
